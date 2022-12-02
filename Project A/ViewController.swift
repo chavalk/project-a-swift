985bc8da02e39a087b68afee261d98ac82ea745f
chavalk <chavalk@hotmail.com>
2022-12-02 00:26:08 -0600

    First commit.

---
+++ Project A/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project A
+//
+//  Created by Jose Garcia on 12/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
