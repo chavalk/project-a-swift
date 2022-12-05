8cc9b8c5d1863b3b4eece99ae7b6c85566e9698a
chavalk <chavalk@hotmail.com>
2022-12-04 22:15:47 -0600

    Add home feed table to main view controller.

--- Project A/MainViewController.swift
+++ Project A/MainViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    private let homeFeedTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemYellow
+        view.addSubview(homeFeedTable)
     }
 
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
+    }
 }
 
