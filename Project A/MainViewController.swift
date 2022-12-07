0c3857f1ae04942a63fded99c2cc78645a0f893b
chavalk <chavalk@hotmail.com>
2022-12-07 12:38:55 -0600

    Reconfigure main view controller and two column table view cell.

--- Project A/MainViewController.swift
+++ Project A/MainViewController.swift
@@ -9,25 +9,25 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    private let homeFeedTable: UITableView = {
-        let table = UITableView(frame: .zero, style: .grouped)
-        table.register(TwoColumnTableViewCell.self, forCellReuseIdentifier: TwoColumnTableViewCell.identifier)
-        return table
-    }()
+    private let homeFeedTable = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        title = "All Tables"
-        view.addSubview(homeFeedTable)
-        
-        homeFeedTable.delegate = self
-        homeFeedTable.dataSource = self
-        
-        let headerView = HeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 30))
-        homeFeedTable.tableHeaderView = headerView
+        configureTableView()
     }
 
+    func configureTableView() {
+        view.addSubview(homeFeedTable)
+        setTableViewDelegates()
+        homeFeedTable.rowHeight = 100
+        // Register cells
+    }
+    
+    func setTableViewDelegates() {
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
+    }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         homeFeedTable.frame = view.bounds
@@ -35,27 +35,11 @@ class MainViewController: UIViewController {
 }
 
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 20
-    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: TwoColumnTableViewCell.identifier, for: indexPath) as? TwoColumnTableViewCell else {
-            return UITableViewCell()
-        }
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        200
-    }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        100
+        return UITableViewCell()
     }
 }
--- Project A/Views/TwoColumnTableViewCell.swift
+++ Project A/Views/TwoColumnTableViewCell.swift
@@ -11,32 +11,31 @@ class TwoColumnTableViewCell: UITableViewCell {
 
     static let identifier = "TwoColumnTableViewCell"
     
-    private let firstLabel: UILabel = {
-        let label = UILabel()
-        return label
-    }()
-    
-    private let secondLabel: UILabel = {
-        let label = UILabel()
-        return label
-    }()
+    private let firstLabel = UILabel()
+    private let secondLabel = UILabel()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.addSubview(firstLabel)
         contentView.addSubview(secondLabel)
         
-        firstLabel.text = "1"
-        secondLabel.text = "Countries By GDP Nominal"
+        setFirstLabelConstraints()
+        setSecondLabelConstraints()
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        firstLabel.frame = CGRect(x: 5, y: 5, width: 10, height: 100)
-        secondLabel.frame = CGRect(x: 40, y: 5, width: 300, height: 100)
     }
     
     required init?(coder: NSCoder) {
         fatalError()
+    }
+    
+    func setFirstLabelConstraints() {
+        firstLabel.frame = CGRect(x: 5, y: 5, width: 10, height: 100)
+    }
+    
+    func setSecondLabelConstraints() {
+        secondLabel.frame = CGRect(x: 40, y: 5, width: 300, height: 100)
     }
 }
