c6928b2046c4c08b46c4893131ec2c6056db56f3
chavalk <chavalk@hotmail.com>
2022-12-07 12:52:54 -0600

    Add dummy data to fill table cells.

--- Project A/MainViewController.swift
+++ Project A/MainViewController.swift
@@ -43,3 +43,17 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
         return UITableViewCell()
     }
 }
+
+extension MainViewController {
+    
+    func fetchData() -> [Row] {
+        let row1 = Row(rank: 1, name: "Countries By GDP Nominal")
+        let row2 = Row(rank: 2, name: "Liga MX Femenil Titles By Club")
+        let row3 = Row(rank: 3, name: "Richest People By Net Worth Annual")
+        let row4 = Row(rank: 4, name: "US Population By Metro")
+        let row5 = Row(rank: 5, name: "Liga MX Varonil Titles By Club")
+        let row6 = Row(rank: 6, name: "FIFA World Cup Titles By Country")
+        
+        return [row1, row2, row3, row4, row5, row6]
+    }
+}
