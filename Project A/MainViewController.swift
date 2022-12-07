1f35bb3f6cc91d4f6521acaad17daa179d0bce38
chavalk <chavalk@hotmail.com>
2022-12-06 21:57:01 -0600

    Reconfigure table in main view controller.

--- Project A/MainViewController.swift
+++ Project A/MainViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class MainViewController: UIViewController {
 
     private let homeFeedTable: UITableView = {
-        let table = UITableView()
+        let table = UITableView(frame: .zero, style: .grouped)
         table.register(TwoColumnTableViewCell.self, forCellReuseIdentifier: TwoColumnTableViewCell.identifier)
         return table
     }()
@@ -37,8 +37,12 @@ class MainViewController: UIViewController {
 }
 
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 20
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 6
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -47,5 +51,13 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
         }
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        200
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        100
     }
 }
