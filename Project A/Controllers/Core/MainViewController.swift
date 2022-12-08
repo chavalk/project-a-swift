71a97aa0e41b77f38382bca9c6b32bd1bcadfe9a
chavalk <chavalk@hotmail.com>
2022-12-07 22:15:54 -0600

    Rearrange folder structure.

--- Project A/Controllers/Core/MainViewController.swift
+++ Project A/Controllers/Core/MainViewController.swift
@@ -50,6 +50,10 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
         cell.set(row: row)
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
 extension MainViewController {
