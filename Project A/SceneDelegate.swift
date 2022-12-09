b186b5aa1645a4cc13b79d7eb12f2c0e5c9e7d10
chavalk <chavalk@hotmail.com>
2022-12-09 01:44:53 -0600

    Remove large title from navigation controller.

--- Project A/SceneDelegate.swift
+++ Project A/SceneDelegate.swift
@@ -16,7 +16,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let navController = UINavigationController(rootViewController: MainViewController())
-        navController.navigationBar.prefersLargeTitles = true
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
