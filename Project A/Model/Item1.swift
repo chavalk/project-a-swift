3cac620afb178225d54ba704383204f649af90d8
chavalk <chavalk@hotmail.com>
2023-01-20 20:17:35 -0600

    Add model for three column table items.

---
+++ Project A/Model/Item1.swift
@@ -0,0 +1,14 @@
+//
+//  Item1.swift
+//  Project A
+//
+//  Created by Jose Garcia on 1/20/23.
+//
+
+import Foundation
+
+struct Item1: Codable {
+    let rank: String
+    let country: String
+    let amount: String
+}
