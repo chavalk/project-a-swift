00e8edc468d9821d3872b0188ff8c314915c7b4a
chavalk <chavalk@hotmail.com>
2023-01-20 20:21:30 -0600

    Add model for three column table.

---
+++ Project A/Model/Table1.swift
@@ -0,0 +1,13 @@
+//
+//  Table1.swift
+//  Project A
+//
+//  Created by Jose Garcia on 1/20/23.
+//
+
+import Foundation
+
+struct Table1: Codable {
+    let tableName: String
+    let tableItems: [Item1]
+}
